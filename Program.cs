de1971efde2ed0d2026b50d1a2ead277dbec5e83
oscar2020-jpg <69723477+oscar2020-jpg@users.noreply.github.com>
2021-06-15 12:22:55 -0500

    ERROR

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace manejador_de_errores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+CATCH int dato1 = 16;
+String dato2 = "25g";
+int resultado = dato1 + Convert.ToInt32(dato2);
